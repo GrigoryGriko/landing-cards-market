a0adf89fc5756d6dca481d3047475a60d7defca3
GrigoryGriko <griko1996@gmail.com>
2023-06-19 16:05:06 +0300

    style: верстка превого slick slide в секции info

--- pages/index.js
+++ pages/index.js
@@ -3,4 +3,11 @@ $('.single-item').slick({
   nextArrow: '<div class="slick-next"></div>',
 });
 
+$('.slick-vertical').slick({
+  vertical: true,
+  verticalSwiping: true,
+  slidesToShow: 3,
 
+  prevArrow: '<div class="slick-prev2"></div>',
+  nextArrow: '<div class="slick-next2"></div>',
+});
