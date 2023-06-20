520f013a5d6a38920f709eedf8354615ce7fdfad
GrigoryGriko <griko1996@gmail.com>
2023-06-20 11:51:55 +0300

    style: верста точек переключения в слайдере  в секции info

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 $('.single-item').slick({
   prevArrow: '<div class="slick-prev"></div>',
-  nextArrow: '<div class="slick-next"></div>',
+  nextArrow: '<div class="slick-next"></div>'
 });
 
 $('.slick-vertical').slick({
@@ -9,5 +9,14 @@ $('.slick-vertical').slick({
   slidesToShow: 3,
 
   prevArrow: '<div class="slick-prev2"></div>',
-  nextArrow: '<div class="slick-next2"></div>',
+  nextArrow: '<div class="slick-next2"></div>'
+});
+
+$('.car-color-slider').slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  dots: true,
+  dotsClass: 'car-color-slick-dots',
 });
